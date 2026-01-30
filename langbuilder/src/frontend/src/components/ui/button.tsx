271a8c4f4a1af4760f88e2c098363ec99c622022
Manas <manasprasad712@gmail.com>
2026-01-30 13:37:02 +0530

    Sidebar colour changed

--- langbuilder/src/frontend/src/components/ui/button.tsx
+++ langbuilder/src/frontend/src/components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground  hover:bg-primary-hover",
+       default:
+  "bg-[var(--button-primary)] text-[var(--button-primary-foreground)] hover:bg-[var(--button-primary-hover)] disabled:bg-[var(--button-primary-disabled)]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
