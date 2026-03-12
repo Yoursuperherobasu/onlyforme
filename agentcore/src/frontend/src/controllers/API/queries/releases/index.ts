66ac55b973fdf02c09615e0899ccef0004c50934
Manas <manasprasad712@gmail.com>
2026-03-12 16:08:53 +0530

    Added RMS,fixes of login page css

--- agentcore/src/frontend/src/controllers/API/queries/releases/index.ts
+++ agentcore/src/frontend/src/controllers/API/queries/releases/index.ts
@@ -1,3 +1,5 @@
-export * from "./use-get-releases";
 export * from "./use-get-current-release";
+export * from "./use-get-release-details";
+export * from "./use-get-releases";
 export * from "./use-post-bump-release";
+export * from "./use-post-bump-release-with-details";
