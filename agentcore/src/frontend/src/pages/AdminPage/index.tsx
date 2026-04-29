d3ee68b8095d04c8db7e32b41b8e961a5252fa30
Manas <manasprasad712@gmail.com>
2026-04-29 13:44:10 +0530

    ISSUE TRACKER FIXES 7"

--- agentcore/src/frontend/src/pages/AdminPage/index.tsx
+++ agentcore/src/frontend/src/pages/AdminPage/index.tsx
@@ -893,7 +893,7 @@ export default function AdminPage() {
             <>
               <div
                 className={
-                  "m-4 h-fit overflow-x-hidden overflow-y-scroll rounded-md border-2 bg-background custom-scroll" +
+                  "m-4 h-fit overflow-x-auto overflow-y-scroll rounded-md border-2 bg-background custom-scroll" +
                   (isPending ? " border-0" : "")
                 }
               >
